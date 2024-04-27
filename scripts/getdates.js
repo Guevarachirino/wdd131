70d1c9cf29f7da2486bb6a7861cca32ea4595416
Guevarachirino <chi23017@byui.edu>
2024-04-27 15:16:44 -0400

    onemore

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+const year = document.querySelector("year");
+
+//data object
+const today = new Date();
+
+year.innerHTML = 'getFullYear(): <span class="highlight">${today.getFullYear()}</span>';
+
+// getting date 
+alert(document.lastModified);
