efea03d21fa4db4ef2aa04c357469fefd8b85882
Guevarachirino <chi23017@byui.edu>
2024-05-03 23:45:17 -0400

    cambios

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,19 +1,46 @@
+// select the DOM elements for output
+const full = document.querySelector("#full");
+const worldfull = document.querySelector("#world-full");
+const short = document.querySelector("#short");
+const medium = document.querySelector("#medium");
 const year = document.querySelector("#year");
+const month = document.querySelector("#month");
+const day = document.querySelector("#day");
+const dayofweek = document.querySelector("#dayofweek");
 
+// use the date object
 const today = new Date();
-const pattern = /last_modif\s*=\s*([^;]*)/;
 
-const lastVisit = parseFloat(document.cookie.replace(pattern, "$1"));
-const lastModif = Date.parse(document.lastModified);
-
-if (Number.isNaN(lastVisit) || lastModif > lastVisit) {
-    document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${location.pathname
-        }`;
-
-    if (isFinite(lastVisit)) {
-        alert("This page has been changed!");
+full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
+    "en-US",
+    {
+        dateStyle: "full"
     }
-}
+).format(today)}</span>`;
+worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat(
+    "en-UK",
+    {
+        dateStyle: "full"
+    }
+).format(today)}</span>`;
+short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
+    "en-US",
+    {
+        dateStyle: "short"
+    }
+).format(today)}</span>`;
+medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
+    "en-US",
+    {
+        dateStyle: "medium"
+    }
+).format(today)}</span>`;
+
+year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
+day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
+dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
+
 
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
