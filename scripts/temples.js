999d7eeb530c5cd9dad86a93bd915d7d5084d5ab
Guevarachirino <chi23017@byui.edu>
2024-05-04 20:15:35 -0400

    new

--- scripts/temples.js
+++ scripts/temples.js
@@ -62,10 +62,11 @@ if (Number.isNaN(lastVisit) || lastModif > lastVisit) {
 //hamb botton
 
 // Store the selected elements that we are going to use. 
-const burgermenu = document.querySelector('.burgermenu')
-const navLinks = document.querySelector('.nav-links');
+const burgermenu = document.querySelector(".burgermenu")
+const navLinks = document.querySelector(".nav-links")
 
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
-burgermenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
+burgermenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+}
+)
