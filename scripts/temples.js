da364d5e037214e8c7ae178315510e1d6a85f343
Guevarachirino <chi23017@byui.edu>
2024-05-04 20:43:21 -0400

    last

--- scripts/temples.js
+++ scripts/temples.js
@@ -46,19 +46,9 @@ dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span
 
 //last modified
 
-const pattern = /last_modif\s*=\s*([^;]*)/;
 
-const lastVisit = parseFloat(document.cookie.replace(pattern, "$1"));
-const lastModif = Date.parse(document.lastModified);
-
-if (Number.isNaN(lastVisit) || lastModif > lastVisit) {
-    document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${location.pathname
-        }`;
-
-    if (isFinite(lastVisit)) {
-        alert("This page has been changed!");
-    }
-}
+const date = new Date(document.lastModified);
+document.getElementById("lastModif").innerHTML = date;
 //hamb botton
 
 // Store the selected elements that we are going to use. 
