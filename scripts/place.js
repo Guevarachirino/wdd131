40c533b2f174845cc2862fe799dd52489ac2c3d9
Guevarachirino <chi23017@byui.edu>
2024-05-09 23:58:07 -0400

    new

--- scripts/place.js
+++ scripts/place.js
@@ -1,6 +1,6 @@
 const year = document.querySelector("#getyear");
 const today = new Date();
-year.innerHTML = `©️ <span class="getyear">${today.getFullYear()}</span>`;
+year.innerHTML = today.getFullYear();
 
 let text = document.lastModified;
 document.getElementById("lastModif").innerHTML = text;
@@ -29,4 +29,21 @@ function calculateWindChill() {
   );
 
 }
+
+const lastModified = document.querySelector('#lastModified');
+lastModified.innerHTML = (document.lastModified);
+
+const temp = 64;
+const windSpeed = 19.71;
+function calculateWindchill(temp, windSpeed) {
+  if (temp <= 50 && windSpeed > 3) {
+    const windchill = 35.74 + 0.6215 * temp - 35.75 * windSpeed ** 0.16 + 0.4275 * temp * windSpeed ** 0.16;
+    const roundedWindchill = Math.round(windchill * 100) / 100;
+    return roundedWindchill;
+  } else {
+    return "N/A";
+  }
+}
+const windchillFactor = document.querySelector('#windchill-factor');
+windchillFactor.textContent = calculateWindchill(temp, windSpeed);
 
