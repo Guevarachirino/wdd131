36e7bc5483eff291c09dd2ebefbed115c67294e5
Guevarachirino <chi23017@byui.edu>
2024-10-02 01:01:30 -0400

    week3

---
+++ scripts/place.js
@@ -0,0 +1,19 @@
+//getting dates and time
+
+const getyear = document.getElementById('year');
+const actualyear = new Date().getFullYear();
+getyear.textContent = actualyear;
+
+
+let text = document.lastModified;
+document.getElementById("oLastModif").innerHTML = text;
+
+function calculateWindChill() {
+    let temp = 6;
+    let windSpeed = 11;
+    return (13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16)).toFixed(2);
+}
+
+let windChill = calculateWindChill();
+
+document.getElementById("wind-chill").textContent = ` ${windChill} ⁰C`;
