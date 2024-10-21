1be0a1b44b3f75c25644a58003d1f22929aba79a
Guevarachirino <chi23017@byui.edu>
2024-10-20 20:47:38 -0400

    new

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,19 @@
+//getting dates and time
+
+const getyear = document.getElementById('year');
+const actualyear = new Date().getFullYear();
+getyear.textContent = actualyear;
+
+
+let text = document.lastModified;
+document.getElementById("oLastModif").innerHTML = text;
+
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
